cd6b267ddcd591d45c73ee7b7de2ac4a1599944b
Krishna Srinivas <krishna.srinivas@gmail.com>
2021-01-04 17:30:48 -0800

    initial commit

---
+++ lb-test-2.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"bytes"
+	"crypto/rand"
+	"flag"
+	"io/ioutil"
+	"log"
+	"net/url"
+	"strings"
+
+	minio "github.com/minio/minio-go"
+)
+
+func main() {
+	endpointsPtr := flag.String("endpoints", "", "endpoint. ex. 192.168.1.20:9000,192.168.1.21:9000,192.168.1.22:9000")
+	accessKeyPtr := flag.String("accesskey", "minioadmin", "access key")
+	secretKeyPtr := flag.String("secretkey", "minioadmin", "secret key")
+	bucketPtr := flag.String("bucket", "", "bucket name")
+
+	flag.Parse()
+
+	endpoints := *endpointsPtr
+	accessKey := *accessKeyPtr
+	secretKey := *secretKeyPtr
+	bucket := *bucketPtr
+
+	if endpoints == "" {
+		log.Fatal("endpoint not provided")
+	}
+	if bucket == "" {
+		log.Fatal("bucket not provided")
+	}
+
+	endpointsSlice := strings.Split(endpoints, ",")
+	var clients []*minio.Core
+
+	for _, e := range endpointsSlice {
+		u, err := url.Parse(e)
+		if err != nil {
+			log.Fatal("url.Parse()", err)
+		}
+		c, err := minio.NewCore(u.Host, accessKey, secretKey, u.Scheme == "https")
+		if err != nil {
+			log.Fatal("minio.NewCore()", err)
+		}
+		clients = append(clients, c)
+	}
+	object := "mpobject"
+	id, err := clients[0].NewMultipartUpload(bucket, object, minio.PutObjectOptions{})
+	if err != nil {
+		log.Fatal("NewMultipartUpload()", err)
+	}
+	partData := make([]byte, 5*1024*1024)
+	_, err = rand.Read(partData)
+	if err != nil {
+		log.Fatal("rand.Read()", err)
+	}
+	var parts []minio.ObjectPart
+	partNum := 1
+	for {
+		for _, c := range clients {
+			if partNum == 101 {
+				break
+			}
+			part, err := c.PutObjectPart(bucket, object, id, partNum, bytes.NewReader(partData), int64(len(partData)), "", "", nil)
+			if err != nil {
+				log.Fatal("c.PutObjectPart()", err)
+			}
+			parts = append(parts, part)
+			partNum++
+		}
+	}
+	var completeParts []minio.CompletePart
+	for _, part := range parts {
+		completeParts = append(completeParts, minio.CompletePart{
+			ETag:       part.ETag,
+			PartNumber: part.PartNumber,
+		})
+	}
+	_, err = clients[0].CompleteMultipartUpload(bucket, object, id, completeParts)
+	if err != nil {
+		log.Fatal("CompleteMultipartUpload()", err)
+	}
+
+	_, err = clients[0].PutObject(bucket, object, bytes.NewReader(partData), int64(len(partData)), "", "", nil, nil)
+	if err != nil {
+		log.Fatal("PutObject()", err)
+	}
+
+	r, info, err := clients[len(clients)-1].GetObject(bucket, object, minio.GetObjectOptions{})
+	if err != nil {
+		log.Fatal("GetObject()", err)
+	}
+	if info.Size != int64(len(partData)) {
+		log.Fatal("GetObject() size not correct")
+	}
+	buf, err := ioutil.ReadAll(r)
+	if err != nil {
+		log.Fatal("ReadAll()", err)
+	}
+	if bytes.Compare(buf, partData) != 0 {
+		log.Fatal("contents differ")
+	}
+	if int64(len(buf)) != info.Size {
+		log.Fatal("dataReadCount != info.Size")
+	}
+}
---
+++ put1-get6.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/url"
+	"path"
+
+	minio "github.com/minio/minio-go"
+)
+
+func main() {
+	endpoint1Ptr := flag.String("endpoint1", "", "endpoint. ex. https://192.168.1.20:9000")
+	endpoint6Ptr := flag.String("endpoint6", "", "endpoint. ex. https://192.168.1.20:9000")
+	accessKeyPtr := flag.String("accesskey", "minioadmin", "access key")
+	secretKeyPtr := flag.String("secretkey", "minioadmin", "secret key")
+	bucketPtr := flag.String("bucket", "", "bucket name")
+	prefixPtr := flag.String("prefix", "", "prefix")
+	localFilePtr := flag.String("f", "", "local file path")
+
+	flag.Parse()
+
+	endpoint1 := *endpoint1Ptr
+	endpoint6 := *endpoint6Ptr
+	accessKey := *accessKeyPtr
+	secretKey := *secretKeyPtr
+	bucket := *bucketPtr
+	prefix := *prefixPtr
+	localFile := *localFilePtr
+
+	if endpoint1 == "" {
+		log.Fatal("endpoint1 not provided")
+	}
+	if endpoint6 == "" {
+		log.Fatal("endpoint6 not provided")
+	}
+	if bucket == "" {
+		log.Fatal("bucket not provided")
+	}
+	if localFile == "" {
+		log.Fatal("local file not provided")
+	}
+
+	localFileContent, err := ioutil.ReadFile(localFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	u, err := url.Parse(endpoint1)
+	if err != nil {
+		log.Fatal("url.Parse()", err)
+	}
+	s3Client1, err := minio.New(u.Host, accessKey, secretKey, u.Scheme == "https")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	u, err = url.Parse(endpoint6)
+	if err != nil {
+		log.Fatal("url.Parse()", err)
+	}
+	s3Client6, err := minio.New(u.Host, accessKey, secretKey, u.Scheme == "https")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	objName := localFile
+	if prefix != "" {
+		objName = path.Join(prefix, localFile)
+	}
+	_, err = s3Client1.PutObject(bucket, objName, bytes.NewBuffer(localFileContent), int64(len(localFileContent)), minio.PutObjectOptions{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	obj, err := s3Client6.GetObject(bucket, objName, minio.GetObjectOptions{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	objContent, err := ioutil.ReadAll(obj)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if bytes.Compare(objContent, localFileContent) != 0 {
+		fmt.Println("contents differ for", objName)
+	}
+}
